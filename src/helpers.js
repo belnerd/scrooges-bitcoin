f85204eace3145fad9030b267ed9afabc0b2e738
AB5123 <ab5123@student.jamk.fi>
2021-11-26 13:48:12 +0200

    Add transform function

--- src/helpers.js
+++ src/helpers.js
@@ -25,12 +25,28 @@ module.exports = {
     let date = new Date(timestamp);
     let dateobj = {
       year: date.getUTCFullYear(),
-      month: date.getUTCMonth()+1,
+      month: date.getUTCMonth() + 1,
       date: date.getUTCDate(),
       hours: date.getUTCHours(),
       minutes: date.getUTCMinutes(),
       seconds: date.getUTCSeconds(),
     };
     return dateobj;
+  },
+  // Transform data gotten from the Coingecko API as daily data
+  // Parameters require an object and it's property to be transformed (prices/market_caps/total_volumes)
+  transformToDaily: (obj, property) => {
+    const data = obj[property];
+    let newData = [[data[0][0]/1000, data[0][1]]];
+    let day = module.exports.formatDateUTCObject(data[0][0]).date; // Set starting point
+    for (let i = 1; i < data.length; i++) {
+      let dayCounter = module.exports.formatDateUTCObject(data[i][0]).date;
+      // Check if the day has changed and push the first marking of the day to an array
+      if (dayCounter !== day) {
+        day = dayCounter;
+        newData.push([data[i][0]/1000, data[i][1]]);
+      }
+    }
+    return newData;
   },
 };
