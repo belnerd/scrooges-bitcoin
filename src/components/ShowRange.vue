d29a8ffb7f59104c9d1ed3ad27a3a2e262f4463e
AB5123 <ab5123@student.jamk.fi>
2021-11-29 14:07:52 +0200

    Find and show best days to buy and sell

--- src/components/ShowRange.vue
+++ src/components/ShowRange.vue
@@ -2,7 +2,7 @@
 process and display that data.
 <template>
   <div>
-    <div v-if="!startDate || !endDate">Please selects dates</div>
+    <div v-if="!startDate || !endDate">Please select dates</div>
     <div v-else-if="rangeData.length === 0">Loading data</div>
     <div v-else>
       <p>Longest downward trend was {{ downDays }} days</p>
@@ -10,15 +10,23 @@ process and display that data.
         Highest volume was {{ volume.volume.toFixed(0) }} EUR at
         {{ volume.date }}
       </p>
+      <div v-if="!hold">
+        <p>
+          You should buy at {{ buyDay.timestamp }} when price is
+          {{ formatCurrency(buyDay.price) }}
+        </p>
+        <p>
+          You should sell at {{ sellDay.timestamp }} when price is
+          {{ formatCurrency(sellDay.price) }}
+        </p>
+      </div>
+      <p v-else>You shouldn't buy or sell during this time period.</p>
     </div>
   </div>
 </template>
 
 <script>
-import {
-  formatDate,
-  transformToDaily,
-} from '../helpers.js';
+import { formatDate, transformToDaily, formatCurrency } from '../helpers.js';
 
 export default {
   name: 'ShowRange',
@@ -29,6 +37,15 @@ export default {
       from: this.startDate,
       to: this.endDate,
       downDays: '',
+      buyDay: {
+        timestamp: '',
+        price: '',
+      },
+      sellDay: {
+        timestamp: '',
+        price: '',
+      },
+      hold: false,
       volume: {},
       prices: {},
     };
@@ -57,16 +74,17 @@ export default {
       this.prices = transformToDaily(this.rangeData, 'prices');
       this.findBearish();
       this.findHighestVolume();
+      this.findTradeDays();
     },
   },
   methods: {
-    // Get data from coingeck API
+    // Get data from Coingecko API
     getData(url) {
       const headers = { Accept: 'application/json' };
       fetch(url, { headers })
         .then((response) => response.json())
         .then((data) => (this.rangeData = data));
-    },    
+    },
     // Find the longest downward trend from the data that has been transformed to daily data
     findBearish() {
       const prices = this.prices;
@@ -121,6 +139,55 @@ export default {
         }),
       };
     },
+    // Find best days to buy (lowest price) and sell (highest price)
+    findTradeDays() {
+      const prices = this.prices;
+      console.log(prices);
+      // Set the min and max price as first values at range
+      let min = {
+        timestamp: prices[0][0],
+        price: prices[0][1],
+      };
+      let max = {
+        timestamp: prices[0][0],
+        price: prices[0][1],
+      };
+      // Set the first difference in daily prices, then compare each element to find maximum difference for best profit
+      let maxDiff = prices[1][1] - prices[0][1];
+      for (let i = 0; i < prices.length; i++) {
+        for (let j = i + 1; j < prices.length; j++) {
+          if (prices[j][1] - prices[i][1] > maxDiff) {
+            maxDiff = prices[j][1] - prices[i][1];
+            min.timestamp = prices[i][0];
+            min.price = prices[i][1];
+            max.timestamp = prices[j][0];
+            max.price = prices[j][1];
+          }
+        }
+      }
+      // Check if price does change or just decreases during the time period
+      if (min.price < max.price) {
+        this.buyDay = {
+          timestamp: formatDate(min.timestamp, 'fi', {
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric',
+          }),
+          price: min.price,
+        };
+        this.sellDay = {
+          timestamp: formatDate(max.timestamp, 'fi', {
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric',
+          }),
+          price: max.price,
+        };
+      } else {
+        this.hold = true;
+      }
+    },
+    formatCurrency,
   },
 };
 </script>
