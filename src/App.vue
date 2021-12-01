bb2ed4281148616c64cdc568737a50c3e6a2141c
AB5123 <ab5123@student.jamk.fi>
2021-12-01 13:24:52 +0200

    Some further styling

--- src/App.vue
+++ src/App.vue
@@ -83,6 +83,57 @@ export default {
   box-sizing: border-box;
 }
 
+html {
+  font-family: arial, sans-serif;
+}
+
+.header,
+.footer {
+  background: linear-gradient(to bottom, #007dc1 5%, #0061a7 100%);
+  color: #ffffff;
+}
+
+.footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+}
+
+.button {
+  margin-top: 10px;
+  box-shadow: inset 0px 1px 0px 0px #54a3f7;
+  background: linear-gradient(to bottom, #007dc1 5%, #0061a7 100%);
+  background-color: #007dc1;
+  border-radius: 3px;
+  border: 1px solid #124d77;
+  display: inline-block;
+  cursor: pointer;
+  color: #ffffff;
+  font-family: Arial;
+  font-size: 13px;
+  padding: 6px 24px;
+  text-decoration: none;
+  text-shadow: 0px 1px 0px #154682;
+}
+.button:hover {
+  background: linear-gradient(to bottom, #0061a7 5%, #007dc1 100%);
+  background-color: #0061a7;
+}
+.button:active {
+  position: relative;
+  top: 1px;
+}
+
+input,
+label {
+  display: block;
+}
+
+input {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 20px;
+  color: black;
+}
 .row::after {
   content: '';
   clear: both;
@@ -129,51 +180,5 @@ export default {
 }
 .col-12 {
   width: 100%;
-}
-
-html {
-  font-family: arial, sans-serif;
-}
-
-.header,
-.footer {
-  background-color: #007dc1;
-  color: #ffffff;
-  padding: 15px;
-}
-
-.button {
-  margin-top: 10px;
-  box-shadow: inset 0px 1px 0px 0px #54a3f7;
-  background: linear-gradient(to bottom, #007dc1 5%, #0061a7 100%);
-  background-color: #007dc1;
-  border-radius: 3px;
-  border: 1px solid #124d77;
-  display: inline-block;
-  cursor: pointer;
-  color: #ffffff;
-  font-family: Arial;
-  font-size: 13px;
-  padding: 6px 24px;
-  text-decoration: none;
-  text-shadow: 0px 1px 0px #154682;
-}
-.button:hover {
-  background: linear-gradient(to bottom, #0061a7 5%, #007dc1 100%);
-  background-color: #0061a7;
-}
-.button:active {
-  position: relative;
-  top: 1px;
-}
-
-input,
-label {
-  display: block;
-}
-
-input {
-  font-family: Arial, Helvetica, sans-serif;
-  font-size: 20px;
 }
 </style>
